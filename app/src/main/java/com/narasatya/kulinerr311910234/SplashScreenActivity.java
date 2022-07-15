ade3cdcdaaffcc041eab4a66d5bc8bdb85cd8555
mastio1836 <prasetyotio775@gmail.com>
2022-07-15 20:17:24 +0700

    Initial commit

---
+++ app/src/main/java/com/narasatya/kulinerr311910234/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.narasatya.kulinerr311910234;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent utamaIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(utamaIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
